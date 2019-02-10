1e03b114a1bebe9f4c25fadd401d6c378ba4fbe9
Ximanta <ximanta.sarma@gmail.com>
2019-02-10 19:08:27 +0530

    Added .travis.yml

--- api-service/src/main/java/com/stackroute/Demo.java
+++ api-service/src/main/java/com/stackroute/Demo.java
@@ -1,4 +1,8 @@
 package com.stackroute;
 
 public class Demo {
+
+    public String getMessage(){
+        return "Hello World";
+    }
 }
---
+++ api-service/src/test/java/com/stackroute/MainTest.java
@@ -0,0 +1,17 @@
+package com.stackroute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainTest {
+    @Test
+    public void contextLoads() {
+    }
+
+}
